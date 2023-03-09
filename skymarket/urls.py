6bdf8ad7c39b2d2ef05ba04c96d6f6a2dd192a13
sergd <videomc1979@gmail.com>
2023-03-09 19:25:45 +0300

    pre-written settings urls.py

--- skymarket/urls.py
+++ skymarket/urls.py
@@ -19,8 +19,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/redoc-tasks', include("redoc.urls")),
+
+    # path to djoser end points
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+
+    # path to our account's app endpoints
+    # path("api/accounts/", include("users.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
