3aa41fbc0077e270734e744642dad0bc3b0a1831
sergd <videomc1979@gmail.com>
2023-03-11 23:32:01 +0300

    pre-written setting users serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -12,9 +12,34 @@ class UserRegistrationSerializer(BaseUserRegistrationSerializer):
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
+    """ Сериализация регистрации пользователя и создания нового. """
     email = serializers.EmailField(max_length=60)
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
     # phone = PhoneNumberField(null=False, blank=False, unique=True, verbose_name='Телефон')
     last_login = serializers.DateTimeField(read_only=True)
-    role = serializers.CharField()
+    # role = serializers.CharField()
+
+    ''' Убедитесь, что пароль содержит не менее 8 символов, не более 128,
+    и так же что он не может быть прочитан клиентской стороной '''
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+    ''' Клиентская сторона не должна иметь возможность отправлять токен вместе с
+    запросом на регистрацию. Сделаем его доступным только на чтение. '''
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        ''' Перечислить все поля, которые могут быть включены в запрос
+        или ответ, включая поля, явно указанные выше. '''
+        fields = ['first_name', 'last_name', 'phone', 'role', 'token']
+
+    def create(self, validated_data):
+        """ Использовать метод create_user, который мы
+        написали ранее, для создания нового пользователя. """
+        return User.objects.create_user(**validated_data)
+
+
