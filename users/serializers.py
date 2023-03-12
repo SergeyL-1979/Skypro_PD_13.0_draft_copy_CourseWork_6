e87f5e5c31a5bf776f12b955d8063a5bd8743192
sergd <videomc1979@gmail.com>
2023-03-12 17:45:33 +0300

    pre-written serializers.py не совсем понимаю как он работает

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,6 @@
 from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
+# from djoser.views import User
+from phonenumber_field.serializerfields import PhoneNumberField
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
@@ -9,6 +11,55 @@ User = get_user_model()
 
 class UserRegistrationSerializer(BaseUserRegistrationSerializer):
     pass
+    # email = serializers.EmailField(max_length=60)
+    # first_name = serializers.CharField(max_length=50)
+    # last_name = serializers.CharField(max_length=50)
+    # phone = PhoneNumberField()
+    # last_login = serializers.DateTimeField(read_only=True)
+    # role = serializers.CharField()
+    # """ Убедитесь, что пароль содержит не менее 8 символов, не более 128,
+    # и так же что он не может быть прочитан клиентской стороной """
+    # password = serializers.CharField(
+    #     max_length=128,
+    #     min_length=8,
+    #     write_only=True
+    # )
+    # ''' Клиентская сторона не должна иметь возможность отправлять токен вместе с
+    # запросом на регистрацию. Сделаем его доступным только на чтение. '''
+    # token = serializers.CharField(max_length=255, read_only=True)
+
+    # class Meta:
+    #     model = User
+    #     fields = ['first_name', 'last_name', 'phone', 'role', 'image', 'token', 'email', 'is_staff']  # '__all__'
+    #     ''' Перечислить все поля, которые могут быть включены в запрос
+    #     или ответ, включая поля, явно указанные выше. '''
+    #     fields = ['first_name', 'last_name', 'phone', 'role', 'token', 'is_staff']
+    #
+    # def create(self, validated_data):
+    #     """ Использовать метод create_user, который мы
+    #     написали ранее, для создания нового пользователя. """
+    #     return User.objects.create_user(**validated_data)
+
+    # email = serializers.EmailField(max_length=60)
+    # first_name = serializers.CharField(max_length=50)
+    # last_name = serializers.CharField(max_length=50)
+    # phone = PhoneNumberField(null=False, blank=False, unique=True, verbose_name='Телефон')
+    # last_login = serializers.DateTimeField(read_only=True)
+    # role = serializers.CharField()
+    # password = serializers.CharField(write_only=True)
+    #
+    # class Meta:
+    #     model = User
+    #     ''' Перечислить все поля, которые могут быть включены в запрос
+    #     или ответ, включая поля, явно указанные выше. '''
+    #     fields = ['first_name', 'last_name', 'phone', 'role', 'is_staff']
+    #
+    # def create(self, validated_data):
+    #     password = validated_data.pop('password')
+    #     user = User(**validated_data)
+    #     user.set_password(password)
+    #     user.save()
+    #     return user
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
@@ -16,9 +67,9 @@ class CurrentUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=60)
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
-    # phone = PhoneNumberField(null=False, blank=False, unique=True, verbose_name='Телефон')
+    phone = PhoneNumberField()
     last_login = serializers.DateTimeField(read_only=True)
-    # role = serializers.CharField()
+    role = serializers.CharField()
 
     ''' Убедитесь, что пароль содержит не менее 8 символов, не более 128,
     и так же что он не может быть прочитан клиентской стороной '''
@@ -33,13 +84,22 @@ class CurrentUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+        # fields = '__all__'
         ''' Перечислить все поля, которые могут быть включены в запрос
         или ответ, включая поля, явно указанные выше. '''
-        fields = ['first_name', 'last_name', 'phone', 'role', 'token']
+        fields = ['first_name', 'last_name', 'phone', 'role', 'is_staff']
 
-    def create(self, validated_data):
-        """ Использовать метод create_user, который мы
-        написали ранее, для создания нового пользователя. """
-        return User.objects.create_user(**validated_data)
-
+    # def create(self, validated_data):
+    #     """ Использовать метод create_user, который мы
+    #     написали ранее, для создания нового пользователя. """
+    #     return User.objects.create_user(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.email = validated_data.get("email", instance.email)
+    #     instance.first_name = validated_data.get("first_name", instance.first_name)
+    #     instance.last_name = validated_data.get("last_name", instance.last_name)
+    #     instance.phone = validated_data.get("phone", instance.phone)
+    #     instance.last_login = validated_data.get("last_login", instance.last_login)
+    #     instance.save()
+    #     return instance
 
